545ff16bdc0393084b5fc21559872bd95fada45b
iarmankhan <armankhan9244@gmail.com>
2020-02-14 23:42:27 +0530

    Updates

---
+++ precache-manifest.8621ae91f58717a689622837dad01e01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4a3e75d3d2227e1355d67cc4d17de3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f63f8bf47ce629838195",
+    "url": "/monsters-rolodex/static/css/main.45376fe5.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f63f8bf47ce629838195",
+    "url": "/monsters-rolodex/static/js/main.b783038a.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
